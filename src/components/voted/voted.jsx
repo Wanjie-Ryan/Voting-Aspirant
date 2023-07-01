eb3a161e6d519bfff1b3a86799a8727a42d6e254
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-01 18:30:40 +0300

    voting

--- src/components/voted/voted.jsx
+++ src/components/voted/voted.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import './voted.css'
 
 
 
 function Voted() {
+
+    const users = [
+        {
+          firstName: "John",
+          lastName: "Doe",
+          id: "123456",
+          county: "Sample County",
+          subCounty: "Sample Sub-County",
+        },
+        {
+          firstName: "John",
+          lastName: "Doe",
+          id: "123456",
+          county: "Sample County",
+          subCounty: "Sample Sub-County",
+        }
+      ];
 
 
   return (
@@ -13,13 +30,42 @@ function Voted() {
     <>
 
 
-    
-    
-    
+            <div className="table-container">
+
+                <table className="user-table">
+
+                <thead>
+
+                    <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>ID</th>
+                    <th>County</th>
+                    <th>Sub-County</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user, index) => (
+                    <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>Customer</td>
+                        <td>{user.firstName}</td>
+                        <td>{user.lastName}</td>
+                        <td>{user.id}</td>
+                        <td>{user.county}</td>
+                        <td>{user.subCounty}</td>
+                    </tr>
+                    ))}
+                </tbody>
+                </table>
+
+            </div>
+
+
     
     </>
-
-
 
 
   )
