8af4e079660ef7f0fefe603d99dd7193688972e6
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-01 17:12:45 +0300

    voting

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './dashboard.css'
+import {Link} from 'react-router-dom'
+
 
 
 
@@ -10,6 +12,39 @@ function Dashboard() {
 
 
     <>
+
+
+        <section className ='dashboard'>
+
+            <div className ='dashboard__container'>
+
+
+                <div className="orders">
+
+                    <h3 className='orders-title'>Who Voted For You</h3>
+
+                    <Link to='/voted'><div className="lm"><button className="next-page">Know Them</button></div></Link>
+
+
+
+                </div>
+
+
+                <div className="reports">
+
+                    <h3 className='orders-title'>Your Competitors</h3>
+
+                    <Link to ='/competitors'><div className="lm"><button className="next-page">Know Them</button></div></Link>
+
+
+                </div>
+
+            </div>
+
+
+
+        </section>
+
 
 
 
