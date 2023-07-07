cecb167bce62b199486b321ef8204e8809a88271
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-07 13:20:13 +0300

    aspirant-integration

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './profile.css'
-
+import aspsimg from '../../assets/icon-votes.jpg'
+import {BsFillPersonFill, BsFillImageFill} from 'react-icons/bs'
 
 
 
@@ -11,6 +12,54 @@ function Profile() {
 
 
     <>
+
+
+        <section className ='main-profile'>
+
+            <div className='profile-container'>
+
+                <div className="profile-pic">
+
+                    <img src= {aspsimg} className ='prof-pic-user' alt ='profile'/>
+
+
+                </div>
+
+                <div className="updating-details">
+
+                    <div className ='update-img'>
+
+
+                        <BsFillImageFill/>
+
+                        <input type ='file' accept='image/*'/>
+
+
+                    </div>
+
+                    <div className ='name'>
+
+                        <BsFillPersonFill/>
+
+                        <input
+                            type="text"
+                            name="Name"
+                            required
+                            placeholder="Enter Your name"
+                        />
+
+                    </div>
+
+
+                </div>
+
+
+
+            </div>
+
+
+
+        </section>
 
 
 
